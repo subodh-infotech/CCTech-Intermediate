ed5a7af6a3a781fdc8fb4317c48563261cb03497
subodh-infotech <34767877+subodh-infotech@users.noreply.github.com>
2022-12-16 09:35:31 +0530

    Update verify.h

--- Preprocessor/verify.h
+++ Preprocessor/verify.h
@@ -2,8 +2,10 @@
 #define VERIFY_H
 #include <iostream>
 #define verify(condition){\
-    if(!(condition)){\
-       std::cerr << __FILE__ <<": " << __LINE__ << ": error: In the " <<__func__ << "(), verification of contidion \"" << #condition << "\" failed." << std::endl;  \
-    } \
+     do{\
+        if(!(condition)){\
+            std::cerr << __FILE__ <<": " << __LINE__ << ": error: In the " <<__func__ << "(), verification of contidion \"" << #condition << "\" failed." << std::endl;  \
+        } \
+    }while(0);\
 }
 #endif
